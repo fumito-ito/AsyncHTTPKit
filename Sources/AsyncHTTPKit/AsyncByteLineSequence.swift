88f9e78327b2023c76dd21f5b7c4e9438b5cdc91
Fumito Ito <weathercook@gmail.com>
2025-08-08 00:48:41 +0900

    use struct instead of protocol to avoid type erase

--- Sources/AsyncHTTPKit/AsyncByteLineSequence.swift
+++ Sources/AsyncHTTPKit/AsyncByteLineSequence.swift
@@ -5,7 +5,28 @@
 //  Created by 伊藤史 on 2025/08/01.
 //
 
-public protocol AsyncByteLineSequence<Base>: AsyncSequence where Element == String {
-    associatedtype Base: AsyncSequence where Base.Element == UInt8
-    func makeAsyncIterator() -> Self.AsyncIterator
+public struct AsyncByteLineSequence: AsyncSequence, Sendable {
+    public typealias Element = String
+    
+    private let _makeAsyncIterator: @Sendable () -> AsyncIterator
+    
+    public init<Wrapped: AsyncSequence>(_ wrapped: Wrapped) where Wrapped.Element == String, Wrapped: Sendable {
+        self._makeAsyncIterator = { AsyncIterator(wrapped.makeAsyncIterator()) }
+    }
+    
+    public func makeAsyncIterator() -> AsyncIterator {
+        _makeAsyncIterator()
+    }
+    
+    public struct AsyncIterator: AsyncIteratorProtocol {
+        private var wrappedIterator: any AsyncIteratorProtocol
+        
+        init<Wrapped: AsyncIteratorProtocol>(_ wrapped: Wrapped) where Wrapped.Element == String {
+            self.wrappedIterator = wrapped
+        }
+        
+        public mutating func next() async throws -> String? {
+            try await wrappedIterator.next() as? String
+        }
+    }
 }
